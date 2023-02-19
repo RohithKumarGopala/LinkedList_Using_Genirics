ec3da48f4367b63527646828ec2b0c547a057984
“rohithgopala44” <rohithgopala44@gmail.com>
2023-02-19 21:07:39 +0530

    Added: Welcome Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,7 @@
+package linkedlsitUsingGenirics;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Linked List Program Using Genirics");
+    }
+}
