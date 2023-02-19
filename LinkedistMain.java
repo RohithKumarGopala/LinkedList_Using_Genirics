7c21452ec4da20c0ea9fddd560bc997b299715f9
“rohithgopala44” <rohithgopala44@gmail.com>
2023-02-19 21:10:28 +0530

    Added: Program Of Creating Simple Linkedlist Using Genirics

---
+++ LinkedistMain.java
@@ -0,0 +1,9 @@
+package linkedlsitUsingGenirics;
+
+public class LinkedistMain {
+
+        public static void main(String[] args) {
+            System.out.println("-----Welcome to DataStructure Program-----");
+        }
+    }
+
